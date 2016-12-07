f6c2a349ae400ba2ff7eaa3aba62a0c202a4040b
Austin Meeker <austinmeeker@me.com>
2016-12-07 16:56:56 -0600

    chaining where exercises complete with %%

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,7 +4,7 @@ show databases;
 
 describe employees;
 
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
+select * from employees where gender = 'M' and (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
 
 select * from employees where last_name like 'E%';
 
@@ -12,10 +12,13 @@ SELECT * from employees where hire_date like '199%';
 
 SELECT * from employees where birth_date like '%12-25';
 
-SELECT * from employees where last_name like  '%q%';
+SELECT * from employees where last_name like '%q%';
 
+select * from employees where last_name like 'E%' or last_name like '%E';
 
+SELECT * from employees where last_name like 'E%E';
 
+select * from employees where last_name like '%q%' and last_name not like '%qu%';
 
 
 
