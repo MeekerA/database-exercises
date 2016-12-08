557bae76cc1984ac12e387327c05b9d3003fac7f
Austin Meeker <austinmeeker@me.com>
2016-12-08 14:46:25 -0600

    Re layout

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,10 +1,16 @@
 use employees;
 
-SELECT count(*), gender from employees where first_name in ('Irena', 'Vidya', 'Maya') group by gender;
+SELECT count(*), gender from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+group by gender;
 
-select concat(first_name,' ',  last_name) from employees where first_name like 'E%' and last_name like '%E';
+select concat(first_name,' ',  last_name) as 'Full Name'
+from employees
+where first_name like 'E%' and last_name like '%E';
 
-SELECT datediff(now(), hire_date) as 'Days working with company', first_name from employees where hire_date like '199%' and birth_date like '%12-25';
+SELECT datediff(now(), hire_date) as 'Days working with company', first_name
+from employees
+where hire_date like '199%' and birth_date like '%12-25';
 
 select count(*), first_name, last_name
 from employees
