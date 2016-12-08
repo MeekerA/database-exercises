1a75083a8b9865cc1f78138c3dce1de38a750ae8
Austin Meeker <austinmeeker@me.com>
2016-12-08 14:44:56 -0600

    Using MySQL Functions Exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,13 @@
+use employees;
+
+SELECT count(*), gender from employees where first_name in ('Irena', 'Vidya', 'Maya') group by gender;
+
+select concat(first_name,' ',  last_name) from employees where first_name like 'E%' and last_name like '%E';
+
+SELECT datediff(now(), hire_date) as 'Days working with company', first_name from employees where hire_date like '199%' and birth_date like '%12-25';
+
+select count(*), first_name, last_name
+from employees
+where last_name like '%q%' and last_name not like '%qu%'
+group by first_name, last_name
+order by count(*) DESC;
